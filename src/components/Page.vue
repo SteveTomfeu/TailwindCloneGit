720606d892dbd01dd5573e96636a77622e80b0a8
Tomfeu Steve <stevefotsing@yahoo.fr>
2022-05-06 10:49:15 +0100

    Structuration de la page

---
+++ src/components/Page.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+
+ <div>  <!--content wraper-->
+    <div>  <!--entete-->
+        <nav>
+            <div>
+                <h1>
+                    <a href="">KamerFood</a>
+                </h1>              
+            </div>
+            <ul>
+                <li>
+                    <a href=""><span>Accueil</span></a>
+                </li>
+                <li>
+                    <a href=""><span>Contact</span></a>
+                </li>
+                <li>
+                    <a href=""><span>Apropos</span></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <main>  <!--contenu principal-->
+        <div>
+            <a href="">Connexion</a>
+            <a href="">Inscription</a>
+        </div>
+        <header>
+                <h2>Recettes</h2>
+                <h3>Pour les pros</h3>
+        </header>
+        <div>
+            <div>
+                <h4>Recettes les plus récentes</h4>
+                <div>
+                    <!--nos cards ici-->
+                    <div>
+                        <img src="#" alt=""/>
+                        <div>
+                            <span>Poisson Braisé</span>
+                            <span>Par tony pro</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h4>Recettes les plus populaires</h4>
+                <div>
+                    <!--Les cartes ici-->
+                </div>
+            </div>
+        </div>
+    </main>
+ </div>
+</template>
